81123e0aac8887cbeb601e13c3872e6f48a5232e
pdev <p.developer0207@gmail.com>
2019-11-14 08:59:29 +1100

    update api url https://9303d922.ngrok.io

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const API_URL = 'https://704b2ff9.ngrok.io';
+const API_URL = 'https://9303d922.ngrok.io';
 
 @Injectable()
 export class TodoService {
